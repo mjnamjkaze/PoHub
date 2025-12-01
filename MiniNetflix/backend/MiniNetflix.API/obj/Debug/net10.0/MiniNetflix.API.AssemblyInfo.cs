e5a9c9637ce683d5dce4d9f2d27f03f5db431fee
NamVH <vo.hai.nam@gosei.com.vn>
2025-12-01 23:17:01 +0700

    feat: initialize frontend application with layout and package configuration

--- MiniNetflix/backend/MiniNetflix.API/obj/Debug/net10.0/MiniNetflix.API.AssemblyInfo.cs
+++ MiniNetflix/backend/MiniNetflix.API/obj/Debug/net10.0/MiniNetflix.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniNetflix.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6784bc3bd19faa7f75e9b05cfaa2c09997717416")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniNetflix.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniNetflix.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
